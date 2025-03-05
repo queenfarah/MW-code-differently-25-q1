ad4f2dae1b6214aa63d8ea9da9d6aa9a84294f9c
Anthony D. Mays <anthonydmays@users.noreply.github.com>
2025-03-04 22:38:39 -0800

    chore: adds missing css and fixes slide for lesson_01 (#29)
    * chore: adds missing css
    
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>
    
    * chore: adds missing css
    
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>
    
    ---------
    
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>

--- slides/src/Slides/Lessons/Lesson01.js
+++ slides/src/Slides/Lessons/Lesson01.js
@@ -59,8 +59,8 @@ function Lesson01() {
                 <h3>How to <em>choose</em> one</h3>
                 <ul>
                     <li style=${{'text-decoration': 'line-through', 'opacity': '.3'}}>Language support</li>
-                    <li style=${{'text-decoration': 'line-through', 'opacity': '.3'}}>Refactoring support</li>
                     <li style=${{'text-decoration': 'line-through', 'opacity': '.3'}}>Extensibility</li>
+                    <li style=${{'text-decoration': 'line-through', 'opacity': '.3'}}>Refactoring support</li>
                     <li>Whatever your boss tells you to use</li>
                 </ul>
             </section>
